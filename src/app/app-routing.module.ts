9dfeb8abcc909336474c217a46c6166410b90617
Conrad Pacesa <conradpacesa@gmail.com>
2017-09-19 23:32:01 -0400

    Added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,16 @@ import { SignUpComponent } from './auth/sign-up.component';
 import { SignInComponent } from './auth/sign-in.component';
 import { ItemListComponent } from './items/item-list.component';
 import { CompletedItemComponent } from './items/completed-item.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'items', component: ItemListComponent },
-  { path: 'completed-items', component: CompletedItemComponent }
+  { path: 'items', component: ItemListComponent, canActivate: [AuthGuard] },
+  { path: 'completed-items', component: CompletedItemComponent, canActivate: [AuthGuard] },
+
+  { path: '**', redirectTo: 'sign-in'}
 ];
 
 @NgModule({
