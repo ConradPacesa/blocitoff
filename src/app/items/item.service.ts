a283c315e317d7cd98d1f05a695455d822d6e835
Conrad Pacesa <conradpacesa@gmail.com>
2017-09-20 23:55:48 -0400

    Refactored item service

--- src/app/items/item.service.ts
+++ src/app/items/item.service.ts
@@ -4,43 +4,54 @@ import { Headers, Http, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { Item } from './item';
+import { User } from '../auth/user';
 
 @Injectable()
 export class ItemService {
 
   private authUrl = 'http://192.168.99.100:3000//users';  // URL to web api
 
-  private currentUser = JSON.parse(localStorage.getItem('currentUser'));
-
   private headers = new Headers({'Content-Type' : 'application/json'});
 
   constructor(private http: Http) { }
 
   getItems(): Promise<Item[]> {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.headers.set('X-User-Email', currentUser.email);
-    this.headers.set('X-User-Token', currentUser.authentication_token);
-    return this.http.get(`${this.authUrl}//${currentUser.id}//items`, {headers: this.headers})
+    let headers = this.getHeaders();
+    let currentUser = this.getCurrentUser();
+    return this.http.get(`${this.authUrl}//${currentUser.id}//items`, {headers: headers})
       .toPromise()
       .then(response => response.json() as Item[])
       .catch(this.handleError);
   }
 
   create(name: String): Promise<Item> {
-    return this.http.post(`${this.authUrl}//${this.currentUser.id}//items`, JSON.stringify({item: {name: name, user_id: this.currentUser.id}}), {headers: this.headers})
+    let currentUser = this.getCurrentUser();
+    return this.http.post(`${this.authUrl}//${currentUser.id}//items`, JSON.stringify({item: {name: name, user_id: currentUser.id}}), {headers: this.headers})
       .toPromise()
       .then((response: Response) => response.json() as Item)
       .catch(this.handleError);
   }
 
   update(item: Item): Promise<Item> {
-    this.headers.set('X-User-Email', this.currentUser.email);
-    this.headers.set('X-User-Token', this.currentUser.authentication_token);
-    return this.http.put(`${this.authUrl}//${this.currentUser.id}//items//${item.id}`, JSON.stringify({item: { completed: true }}), {headers: this.headers})
+    let headers = this.getHeaders();
+    let currentUser = this.getCurrentUser();
+    return this.http.put(`${this.authUrl}//${currentUser.id}//items//${item.id}`, JSON.stringify({item: { completed: true }}), {headers: headers})
       .toPromise()
       .then((response: Response) => response.json() as Item)
       .catch(this.handleError);
   }
+
+  private getCurrentUser(): any {
+    return JSON.parse(localStorage.currentUser);
+  }
+
+  private getHeaders(): Headers {
+    let currentUser = this.getCurrentUser();
+    this.headers.set('X-User-Email', currentUser.email);
+    this.headers.set('X-User-Token', currentUser.authentication_token);
+    return this.headers;
+  }
+
 
   private handleError(error: any): Promise<any> {
     console.error('an error occurred', error);
